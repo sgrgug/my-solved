7dcb445693ad4c336f677af8554a5069ff5ade1b
Sagar Gurung <sgrgug@gmail.com>
2023-10-19 23:20:30 +0545

    resolve solution of power_of_two

--- easy/power_of_two.php
+++ easy/power_of_two.php
@@ -1,33 +1,37 @@
 <?php
 
-class Solution {
+class Solution
+{
 
     /**
      * @param Integer $n
      * @return Boolean
      */
-    function isPowerOfTwo($n) {
-        
-        $counter = 0;
+    function isPowerOfTwo($n)
+    {
 
-        while($n > 1)
-        {
-            if($n % 2 == 0)
-            {
-                $n = $n/2;
-                $counter++;
-            } else {
-                return 'false';
+        if ($n <= 0) {
+            return false;
+        } else {
+
+            $counter = 0;
+
+            while ($n > 1) {
+                if ($n % 2 == 0) {
+                    $n = $n / 2;
+                    $counter++;
+                } else {
+                    return false;
+                }
             }
+
+            return true;
         }
-
-        return 'true';
-
     }
 }
 
 $solution = new Solution();
 
-$n = 2;
+$n = 0;
 
 print_r($solution->isPowerOfTwo($n));
