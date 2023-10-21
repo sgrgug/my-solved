b62f1714b5589acd116972e525b57dbe5d369e5d
Sagar Gurung <sgrgug@gmail.com>
2023-10-21 11:31:57 +0545

    unpub intersection of two array

---
+++ easy/unpub_intersection_of_two_array.php
@@ -0,0 +1,37 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Integer[]
+     */
+    function intersection($nums1, $nums2) {
+
+        $final = [];
+
+        for($i = 0; $i < count($nums1); $i++)
+        {
+            if(in_array($nums1[$i], $nums2) && !in_array($nums1[$i], $final))
+            {
+                $final[] = $nums1[$i];
+            }
+        }
+
+        echo "<pre>";
+        return $final;
+        echo "</pre>";
+
+    }
+}
+
+$solution = new Solution();
+
+// $nums1 = [1, 2, 2, 1];
+// $nums2 = [2, 2];
+
+$nums1 = [9, 4, 9, 8, 4];
+$nums2 = [4, 9, 5];
+
+print_r($solution->intersection($nums1, $nums2));
