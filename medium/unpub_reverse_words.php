7a54bcf0b3efaf3da0d0915149dd304858fc10f7
Sagar Gurung <sgrgug@gmail.com>
2023-10-23 19:45:08 +0545

    reverse_word (medium)

---
+++ medium/unpub_reverse_words.php
@@ -0,0 +1,26 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function reverseWords($s) {
+
+        $s = preg_replace('/\s+/', ' ', $s);
+        $s = explode(" ", $s);
+        $s = array_reverse($s);
+
+        $s = implode(" ", $s);
+
+        return $s;
+    }
+}
+
+$solution = new Solution();
+
+// $s = "a good   example";
+$s = "  hello world  ";
+
+print_r($solution->reverseWords($s));
