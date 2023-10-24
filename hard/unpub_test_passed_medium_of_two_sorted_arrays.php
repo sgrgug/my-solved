ede3850aba508f82b1797cf65a92fb862505951a
Sagar Gurung <sgrgug@gmail.com>
2023-10-24 13:17:46 +0545

    palindrome_number easy submitted

--- hard/unpub_test_passed_medium_of_two_sorted_arrays.php
+++ hard/unpub_test_passed_medium_of_two_sorted_arrays.php
@@ -11,10 +11,28 @@ class Solution {
 
         $twoMergeArray = array_merge($nums1, $nums2);
 
+        $twoMergeArray = (int)implode("", $twoMergeArray);
 
-        $count = count($twoMergeArray);
+        $twoMergeArray = (string)$twoMergeArray;
+
+
+        if($twoMergeArray == 0)
+        {
+            $count = -1;
+        } else {
+            $twoMergeArray = str_split($twoMergeArray);
+            
+            $count = count($twoMergeArray);
+        }
         
-        $medium = ($count + 1) / 2;
+        // return $twoMergeArray;
+
+
+
+
+        // return $count;
+        
+        $medium = (($count + 1) / 2);
 
         return (float)$medium;
         
@@ -25,7 +43,10 @@ class Solution {
 
 $solution = new Solution();
 
-$nums1 = [1, 2];
-$nums2 = [3, 4];
+// $nums1 = [1, 2];
+// $nums2 = [3, 4];
+
+$nums1 = [0, 0];
+$nums2 = [0, 0];
 
 print_r($solution->findMedianSortedArrays($nums1, $nums2));
