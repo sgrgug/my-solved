6f88d9362fc63339fefcfc33cd6650561c6fa66f
Sagar Gurung <sgrgug@gmail.com>
2023-11-02 23:05:13 +0545

    solve third_maximu_number (easy)

--- easy/third_maximum_number.php
+++ easy/third_maximum_number.php
@@ -1,30 +1,40 @@
 <?php
 
-class Solution {
-
+class Solution
+{
     /**
      * @param Integer[] $nums
      * @return Integer
      */
-    function thirdMax($nums) {
-        
+    function thirdMax($nums)
+    {
+
         $nums = array_unique($nums);
 
         sort($nums);
-        $max = max($nums);
 
-        for($i = 0; $i < count($nums); $i++)
+        if(array_key_exists(count($nums)-3, $nums))
         {
-            if($nums[$i] <= $max);
+            return $nums[count($nums)-3];
+        } elseif (array_key_exists(count($nums)-2, $nums)) {
+            return $nums[count($nums)-1];
+        } elseif (array_key_exists(count($nums)-1, $nums)) {
+            return $nums[count($nums)-1];
+        } else {
+            return $nums[0];
         }
+         
+        return $nums[count($nums)-3];
 
-        return $max;
 
+        return $nums;
     }
 }
 
 $solution = new Solution();
 
-$nums = [3,4,3,2,1];
+// $nums = [3, 4, 3, 2, 1];
+// $nums = [3, 4, 3, 2, 1, 5, 6];
+$nums = [1,1,1];
 
 print_r($solution->thirdMax($nums));
